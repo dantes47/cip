d68573d1445ddd78ec74be132425e711d5373d5b
btkachuk <dantes817@gmail.com>
2021-10-09 12:15:05 +0300

    was added basic Customer's CRUD/psql DB

---
+++ app/views/customers/_customer.json.jbuilder
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+json.extract! customer, :id, :first_name, :last_name, :email, :date_of_birth, :created_at, :updated_at
+json.url customer_url(customer, format: :json)
---
+++ db/migrate/20211009090552_create_customers.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class CreateCustomers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :customers do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :date_of_birth
+
+      t.timestamps
+    end
+  end
+end
