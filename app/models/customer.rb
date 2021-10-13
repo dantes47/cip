6dfd47f4b799b81b597a01990fa72344cb9c2442
btkachuk <dantes817@gmail.com>
2021-10-13 22:59:03 +0300

    tested CSV import/ to be continue..

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -19,16 +19,18 @@ class Customer < ApplicationRecord
   # import CSV
   def self.import(data)
     CSV.foreach(data.path, headers: true) do |row|
-      # customer = Customer.create!(row.to_hash)
       hashed_data = row.to_hash
+
       customer = find_or_create_by!(
         first_name: hashed_data['first_name'],
-        email: hashed_data['email']
+        last_name: hashed_data['last_name'],
+        email: hashed_data['email'],
+        date_of_birth: hashed_data['date_of_birth']
       )
 
       customer.update!(hashed_data)
     end
-  rescue StandardError => e
-    Rails.logger.debug(e.message) # rescue, in case - user/admin will deside to push empty dataset.
+  rescue StandardError => err
+    Rails.logger.debug(err.message) # rescue, in case - user/admin will deside to push empty dataset.
   end
 end
