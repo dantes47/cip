5c65fae992b9e90323b224cd974a615b2bff35ae
btkachuk <dantes817@gmail.com>
2021-10-15 06:51:01 +0300

    testing/work with sidqkiq

--- app/workers/import_worker.rb
+++ app/workers/import_worker.rb
@@ -6,15 +6,15 @@ class ImportWorker
   include Sidekiq::Worker
   sidekiq_options retry: false
 
-  def perform(data)
+  def perform(_data)
     # CSV.foreach(data.path, headers: true) do |row|
     #   Customer.create!(row.to_hash)
     # end
   end
 
   def DestroyWorker
-    include Sidekiq::Worker
-    sidekiq_options retry: false
+    include(Sidekiq::Worker)
+    sidekiq_options(retry: false)
 
     def perform
       # Customer.destroy_all
